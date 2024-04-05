76537a2e6e030e9402810231683308e15ec4f46b
Beaskyy <afeez.akintobi@yahoo.com>
2024-04-05 21:00:29 +0100

    connect to database

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,3 +1,16 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// let cached = mongoose || 
+const MONGOBD_URI = process.env.MONGOBD_URI
+
+let cached = (global as any).mongoose || {conn: null, promise: null}
+
+export const connectToDatabase =  async () => {
+  if (!cached.conn) {
+    cached.conn = await mongoose.connect(MONGOBD_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    cached.promise = cached.conn.promise()
+  }
+  return cached.promise
+}
